bfb7e750cb9f4a0726933eeaddc5e6eebeed5088
= <cameron.andrews@gmail.com>
2021-04-28 01:09:48 -0400

    Misc Additions to the Example

--- src/example.cc
+++ src/example.cc
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include <exception>
 
 using json = nlohmann::json;
 
@@ -15,55 +16,58 @@ void print_to_console(std::string str)
 int main()
 {
 
-    json emptyJSON = {};
-
-    double bid = 0;
-    double ask = 0;
-    double aum = 1000;
-    double fees = .001;
-    int count = 0;
-
-    ftx::WSClient client;
-
-    client.subscribe_ticker("BCH/USD");
-
-    client.on_message([&](std::string j) {
-        json tick = json::parse(j);
-        json data = tick.value("data", emptyJSON);
-
-        if (data != emptyJSON && data != NULL)
-        {
-            bid = data.at("bid");
-            ask = data.at("ask");
-        }
-    });
-
-    std::thread ws_thread(&ftx::WSClient::connect, &client);
-
-    while (0 < 1)
+    try
     {
+        json emptyJSON = {};
+        double bid = 0;
+        double ask = 0;
+        double aum = 1000;
+        double fees = .001;
 
-        double size = aum / ask;
-        double profit = MM::mmIsProfitable(fees, bid, ask, size);
+        ftx::WSClient client;
 
-        count++;
+        client.subscribe_ticker("BCH/USD");
 
-        if (profit > 0)
+        client.on_message([&](std::string j) {
+            json tick = json::parse(j);
+            json data = tick.value("data", emptyJSON);
+
+            if (data != emptyJSON && data != NULL)
+            {
+                bid = data.at("bid");
+                ask = data.at("ask");
+            }
+        });
+
+        std::thread ws_thread(&ftx::WSClient::connect, &client);
+
+        while (aum < 2000)
         {
-            aum += profit;
 
-            std::thread t1(print_to_console, "Market Make Possible! Profit: " + std::to_string(profit) + "\n");
-            t1.join();
-        }
-        else
-        {
-            if (count % 10 == 0)
+            double size = aum / ask;
+            double profit = MM::mmIsProfitable(fees, bid, ask, size);
+
+            if (profit > 0)
+            {
+                aum += profit;
+                std::thread t1(print_to_console, "Market Make Possible! Profit: " + std::to_string(profit) + "\n");
+                t1.join();
+            }
+            else
             {
                 std::thread t2(print_to_console, "AUM: " + std::to_string(aum) + "\n");
                 t2.join();
             }
         }
+
+        ws_thread.detach();
+        std::cout << "Final AUM: " + std::to_string(aum) + "\n";
+    }
+    catch (...)
+    {
+        std::cout << "Oops! Exiting the Program";
     }
 
-    ws_thread.join();
+    std::cout << "Goodbye!\n";
+    return 0;
 }
